eb69721ba8892568e3f2c80acec4a8d285eaf99b
KING-BOT-OFFICIAL <103124323+KING-BOT-OFFICIAL@users.noreply.github.com>
2022-05-11 15:08:04 +0530

    [🇱🇰𝙺𝙸𝙽𝙶 𝙱𝙾𝚃🤘] V1

--- Trex/StringSession.js
+++ Trex/StringSession.js
@@ -13,7 +13,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('ALPHA_SESSION' in process.env && string === undefined) {
+        if ('KINGBOT_SESSION' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'Alpha;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'KINGBOT;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
