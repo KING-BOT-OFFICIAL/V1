897571f8afe64814fdf136bdf092fadb8b19167d
KING-BOT-OFFICIAL <103124323+KING-BOT-OFFICIAL@users.noreply.github.com>
2022-05-11 19:38:32 +0530

    Alive msg changed😎🤘

--- plugins/system.js
+++ plugins/system.js
@@ -10,9 +10,9 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('system_stats');
 
-const all = `\n\n\n💠️ 𝙶𝙸𝚃𝙷𝚄𝙱 𝙻𝙸𝙽𝙺 ↣ https://github.com/KING-BOT-OFFICIAL/KING-BOT\n\n💠 𝚈𝙾𝚄𝚃𝚄𝙱𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ↣ \n\n💠 𝙾𝚆𝙽𝙴𝚁 𝙽𝚄𝙼𝙱𝙴𝚁 ↣ https://wa.me/94729352830`
+const all = `\n\n\n💠️ 𝙶𝙸𝚃𝙷𝚄𝙱 𝙻𝙸𝙽𝙺 ↣ https://github.com/KING-BOT-OFFICIAL/KING-BOT \n\n💠 𝚈𝙾𝚄𝚃𝚄𝙱𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ↣ \n\n 💠 𝙾𝚆𝙽𝙴𝚁 𝙽𝚄𝙼𝙱𝙴𝚁 ↣ https://wa.me/94729352830`
 
-const about  = `\n\n\n🔹 𝙽𝙰𝙼𝙴 ↠ [🇱🇰𝙺𝙸𝙽𝙶 𝙱𝙾𝚃🤘]\𝚗🔹𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁 ↠ ᴡʜɪᴛᴇ ʜᴀᴄᴋᴇʀꜱ\𝚗🔹 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 ↠ 1.0.0 ᴘᴜʙʟɪᴄ\𝚗🔹 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 ↠ ꜱɪɴʜᴀʟᴀ & ᴇɴɢʟɪꜱʜ\𝚗🔹 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 ↠ ɴᴏᴅᴇ.ᴊꜱ`
+const about  = `\n\n\n🔹 𝙽𝙰𝙼𝙴 ↠ [🇱🇰𝙺𝙸𝙽𝙶 𝙱𝙾𝚃🤘] \𝚗🔹𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁 ↠ ᴡʜɪᴛᴇ ʜᴀᴄᴋᴇʀꜱ \𝚗🔹 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 ↠ 1.0.0 ᴘᴜʙʟɪᴄ \𝚗🔹 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 ↠ ꜱɪɴʜᴀʟᴀ & ᴇɴɢʟɪꜱʜ \𝚗🔹 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 ↠ ɴᴏᴅᴇ.ᴊꜱ`
 
 const owner = `\n\n\n️`
 
@@ -35,7 +35,7 @@ if (Config.WORKTYPE == 'private') {
  ]
 
 const btn = {
-    contentText: "  ```🙋Hey There! I'm Online now📶️️```\n\n⚜️ My name : [🇱🇰𝙺𝙸𝙽𝙶 𝙱𝙾𝚃🤘]️\n\n ⚜️ Developer: White hackers\n\n⚜️ Thank You For Using [🇱🇰𝙺𝙸𝙽𝙶 𝙱𝙾𝚃🤘]",
+    contentText: "┌─────────────┐\n│       𝙷𝙸 𝚃𝙷𝙴𝚁𝙴,     │\n│  °•ɪ ᴀᴍ ᴀʟɪᴠᴇ ɴᴏᴡ•°  │\n└─────────────┘\n\n\n📌හලෝ කට්ටිය මම මේ වෙලාවේ ᴀʟɪᴠᴇ බම්.\n📌පහල තියෙන ටික කියවලා බලන්න.\n📌නැත්තන් ඔයාට ඕන දෙය එන්න දාලා කියවන්න.\n\n😀ɢᴏᴏᴅ ʟᴜᴄᴋ✌️\n\n\n📌 𝙸 𝚊𝚖 *𒀽᭕￭ᴋᷜɪͥɴᷠɢᷚ￭᭄𒀽* 𝚆𝚑𝚊𝚝𝚜𝚊𝚙𝚙 𝚄𝚜𝚎𝚛 𝙱𝚘𝚝 V1.0.0\n\n📌 𝙺𝚒𝚗𝚐 𝙱𝚘𝚝 𝙸𝚜 𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚍 𝙱𝚢 𝚆𝙷𝙸𝚃𝙴 𝙷𝙰𝙲𝙺𝙴𝚁𝚂.\n\n\n\n💢𝐂𝐎𝐍𝐓𝐀𝐂𝐓 𝐒𝐔𝐏𝐏𝐎𝐑𝐓💢\n\n🎲 𝙶𝙸𝚃𝙷𝚄𝙱 𝙿𝙰𝙶𝙴 ➙ https://github.com/nethsaragimhan/king-bot\n\n🎲 𝚈𝙾𝚄𝚃𝚄𝙱𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ➙ https://youtube.com/channel/UCgwWV1Cya4_gUFKYOQYQtHw\n\n🎲 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃 𝙶𝚁𝙾𝚄𝙿 ➙ https://chat.whatsapp.com/FxRkE50toG4KtC8p9703PY\n\n🎲 𝚃𝙰𝙻𝙴𝙶𝚁𝙰𝙼 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙶𝚁𝙾𝚄𝙿 ➙ https://t.me/kingwabotsupport\n\n🎲 𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙱𝙾𝚃 𝙲𝚁𝙴𝙰𝚃𝙴𝚁 𝙸𝙽 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 ➙ https://wa.me/94729352830\n\n\n\n♦️දැන් මේක කියෙව්වා ඇති.😊\n♦️තාම [🇱🇰𝐊𝐢𝐧𝐠 𝐁𝐨𝐭✌️] පණපිටින් ඉන්නවා.💝\n♦️ගන්න ඕන දෙයක් ගන්න.✌️ \n♦️Command ටික දන්නෙ නැත්තන් ```.menu``` දාන්න.🤗\n\n\n\n    🔥ᴋɪɴɢ ʙᴏᴛ භාවිතය ගැන ස්තූතියි.🔥\n\n      🔰 ꜰᴏʀ 24/7 ʜᴇʟᴘ ᴍꜱɢ ᴍᴇ 🔰",
     footerText: '</> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋɪɴɢ ʙᴏᴛ </>️ ',
     buttons: buttons,
     headerType: 4,
@@ -126,7 +126,7 @@ else if (Config.WORKTYPE == 'public') {
 
 const btn = {
    // imageMessage: 'https://telegra.ph/file/cd35928cef2be17d339c1.jpg',
-    contentText: "```🙋Hey There! I'm Online now📶️️```\n\n⚜️ My name : [🇱🇰𝙺𝙸𝙽𝙶 𝙱𝙾𝚃🤘]️\n\n ⚜️ Developer: White hackers\n\n⚜️ Thank You For Using [🇱🇰𝙺𝙸𝙽𝙶 𝙱𝙾𝚃🤘]",
+    contentText: "┌─────────────┐\n│       𝙷𝙸 𝚃𝙷𝙴𝚁𝙴,     │\n│  °•ɪ ᴀᴍ ᴀʟɪᴠᴇ ɴᴏᴡ•°  │\n└─────────────┘\n\n\n📌හලෝ කට්ටිය මම මේ වෙලාවේ ᴀʟɪᴠᴇ බම්.\n📌පහල තියෙන ටික කියවලා බලන්න.\n📌නැත්තන් ඔයාට ඕන දෙය එන්න දාලා කියවන්න.\n\n😀ɢᴏᴏᴅ ʟᴜᴄᴋ✌️\n\n\n📌 𝙸 𝚊𝚖 *𒀽᭕￭ᴋᷜɪͥɴᷠɢᷚ￭᭄𒀽* 𝚆𝚑𝚊𝚝𝚜𝚊𝚙𝚙 𝚄𝚜𝚎𝚛 𝙱𝚘𝚝 V1.0.0\n\n📌 𝙺𝚒𝚗𝚐 𝙱𝚘𝚝 𝙸𝚜 𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚍 𝙱𝚢 𝚆𝙷𝙸𝚃𝙴 𝙷𝙰𝙲𝙺𝙴𝚁𝚂.\n\n\n\n💢𝐂𝐎𝐍𝐓𝐀𝐂𝐓 𝐒𝐔𝐏𝐏𝐎𝐑𝐓💢\n\n🎲 𝙶𝙸𝚃𝙷𝚄𝙱 𝙿𝙰𝙶𝙴 ➙ https://github.com/nethsaragimhan/king-bot\n\n🎲 𝚈𝙾𝚄𝚃𝚄𝙱𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ➙ https://youtube.com/channel/UCgwWV1Cya4_gUFKYOQYQtHw\n\n🎲 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃 𝙶𝚁𝙾𝚄𝙿 ➙ https://chat.whatsapp.com/FxRkE50toG4KtC8p9703PY\n\n🎲 𝚃𝙰𝙻𝙴𝙶𝚁𝙰𝙼 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙶𝚁𝙾𝚄𝙿 ➙ https://t.me/kingwabotsupport\n\n🎲 𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙱𝙾𝚃 𝙲𝚁𝙴𝙰𝚃𝙴𝚁 𝙸𝙽 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 ➙ https://wa.me/94729352830\n\n\n\n♦️දැන් මේක කියෙව්වා ඇති.😊\n♦️තාම [🇱🇰𝐊𝐢𝐧𝐠 𝐁𝐨𝐭✌️] පණපිටින් ඉන්නවා.💝\n♦️ගන්න ඕන දෙයක් ගන්න.✌️ \n♦️Command ටික දන්නෙ නැත්තන් ```.menu``` දාන්න.🤗\n\n\n\n    🔥ᴋɪɴɢ ʙᴏᴛ භාවිතය ගැන ස්තූතියි.🔥\n\n      🔰 ꜰᴏʀ 24/7 ʜᴇʟᴘ ᴍꜱɢ ᴍᴇ 🔰",
     footerText: '</> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋɪɴɢ ʙᴏᴛ </>️ ',
     buttons: buttons,
     headerType: 4,
@@ -142,7 +142,7 @@ const btn = {
         await new Promise(r => setTimeout(r, 1000));
 
  
-      //  await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: "  ```🙋Hey There! I'm Online now📶️️```\n\n⚜️ My name : [🇱🇰𝙺𝙸𝙽𝙶 𝙱𝙾𝚃🤘]️\n\n ⚜️ Developer: White hackers\n\n⚜️ Thank You For Using [🇱🇰𝙺𝙸𝙽𝙶 𝙱𝙾𝚃🤘]️"})
+      //  await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: "┌─────────────┐\n│       𝙷𝙸 𝚃𝙷𝙴𝚁𝙴,     │\n│  °•ɪ ᴀᴍ ᴀʟɪᴠᴇ ɴᴏᴡ•°  │\n└─────────────┘\n\n\n📌හලෝ කට්ටිය මම මේ වෙලාවේ ᴀʟɪᴠᴇ බම්.\n📌පහල තියෙන ටික කියවලා බලන්න.\n📌නැත්තන් ඔයාට ඕන දෙය එන්න දාලා කියවන්න.\n\n😀ɢᴏᴏᴅ ʟᴜᴄᴋ✌️\n\n\n📌 𝙸 𝚊𝚖 *𒀽᭕￭ᴋᷜɪͥɴᷠɢᷚ￭᭄𒀽* 𝚆𝚑𝚊𝚝𝚜𝚊𝚙𝚙 𝚄𝚜𝚎𝚛 𝙱𝚘𝚝 V1.0.0\n\n📌 𝙺𝚒𝚗𝚐 𝙱𝚘𝚝 𝙸𝚜 𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚍 𝙱𝚢 𝚆𝙷𝙸𝚃𝙴 𝙷𝙰𝙲𝙺𝙴𝚁𝚂.\n\n\n\n💢𝐂𝐎𝐍𝐓𝐀𝐂𝐓 𝐒𝐔𝐏𝐏𝐎𝐑𝐓💢\n\n🎲 𝙶𝙸𝚃𝙷𝚄𝙱 𝙿𝙰𝙶𝙴 ➙ https://github.com/nethsaragimhan/king-bot\n\n🎲 𝚈𝙾𝚄𝚃𝚄𝙱𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ➙ https://youtube.com/channel/UCgwWV1Cya4_gUFKYOQYQtHw\n\n🎲 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃 𝙶𝚁𝙾𝚄𝙿 ➙ https://chat.whatsapp.com/FxRkE50toG4KtC8p9703PY\n\n🎲 𝚃𝙰𝙻𝙴𝙶𝚁𝙰𝙼 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙶𝚁𝙾𝚄𝙿 ➙ https://t.me/kingwabotsupport\n\n🎲 𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙱𝙾𝚃 𝙲𝚁𝙴𝙰𝚃𝙴𝚁 𝙸𝙽 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 ➙ https://wa.me/94729352830\n\n\n\n♦️දැන් මේක කියෙව්වා ඇති.😊\n♦️තාම [🇱🇰𝐊𝐢𝐧𝐠 𝐁𝐨𝐭✌️] පණපිටින් ඉන්නවා.💝\n♦️ගන්න ඕන දෙයක් ගන්න.✌️ \n♦️Command ටික දන්නෙ නැත්තන් ```.menu``` දාන්න.🤗\n\n\n\n    🔥ᴋɪɴɢ ʙᴏᴛ භාවිතය ගැන ස්තූතියි.🔥\n\n      🔰 ꜰᴏʀ 24/7 ʜᴇʟᴘ ᴍꜱɢ ᴍᴇ 🔰️"})
             
         await message.client.sendMessage (message.jid, btn, MessageType.buttonsMessage)
 
